fbcf20d7b924cdce790a4724722a4434c1005799
moham <mohaemdattar8300@gmail.com>
2026-01-12 14:17:20 +0100

    finishing touches

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -18,24 +18,34 @@ def save_to_html(html_data,file_path):
         handle.write(html_data)
 
 
+def serialize_animal(animal_obj):
+    output_obj = ''  # define an empty string
+
+    output_obj += '<li class="cards__item">'
+    output_obj += f'<div class="card__title">{animal_obj['name']}</div>'
+    output_obj += '<p class="card__text">'
+    output_obj += '<ul class="animal__items">'
+    output_obj += f'<li class="diet_type"><strong>Diet:  </strong>{animal_obj['characteristics']['diet']}</li>\n'
+    output_obj += f'<li class="animal__country"><strong>Location:  </strong>{animal_obj['locations'][0]}</li>\n'
+
+    # append information to each string
+    if 'type' in animal_obj['characteristics']:
+        output_obj += f'<li class="fox__type"><strong>Type:  </strong>{animal_obj['characteristics']['type']} </li>\n'
+    if 'group' in animal_obj['characteristics']:
+        output_obj += f'<li class="fox__group"><strong>Group:  </strong>{animal_obj['characteristics']['group']}</li>\n'
+    output_obj += '</ul>'
+    output_obj += '</p>'
+    output_obj += '</li>'
+    return output_obj
+
+
 animals_data = load_data('animals_data.json')
 animals_html_data = load_html_data('animals_template.html')
 
+
 output = ''  # define an empty string
-
 for animal in animals_data:
-    output += '<li class="cards__item">'
-    output += f'<div class="card__title">{animal['name']}</div>\n'
-    output += '<p class="card__text">'
-    output += f'<strong>Diet:</strong>{animal['characteristics']['diet']}<br/>\n'
-    output += f'<strong>Location:</strong>{animal['locations'][0]}<br/>\n'
+    output += serialize_animal(animal)
 
-    # append information to each string
-    if 'type' in animal['characteristics']:
-        output += f'<strong>Type:</strong>{animal['characteristics']['type']} <br/>\n'
-    output += '</p>'
-    output += '</li>'
 new_html_data = animals_html_data.replace('__REPLACE_ANIMALS_INFO__', output)
-print(new_html_data)
-
 save_to_html(new_html_data, 'animals_template.html')
